83ab6ffbd56ad5f30f71738f0229ec699f3c8c5b
Cindy <cindy.cnockaert@live.fr>
2021-10-22 00:07:33 +0200

    maj

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Contact = () => {
+    return (
+        <>
+        <Navigation/>
+            Contact
+        </>
+    );
+};
+
+export default Contact;
